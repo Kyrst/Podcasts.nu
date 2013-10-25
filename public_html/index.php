37e2b9a2db93ff2582b8c1706b4a5959db78f080
Myrberg <dmyrberg@gmail.com>
2013-10-25 11:21:48 +0200

    Testar

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__ . '/../bootstrap/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/start.php';
+$app = require_once __DIR__ . '/../bootstrap/start.php';
 
 $app->run();
 
