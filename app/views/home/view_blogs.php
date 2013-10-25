96b9ada30b70e14514dce77a38622f0849e75122
Myrberg <dmyrberg@gmail.com>
2013-10-25 11:38:04 +0200

    Laddade upp view_blogs

---
+++ app/views/home/view_blogs.php
@@ -0,0 +1,52 @@
+<ul class="breadcrumb">
+    <li><a href="<?= URL::route('home', array(), ''); ?>">Podcasts.nu</a></li>
+    <li>Bloggar</li>
+</ul>
+
+<!-- Presentation av bloggare. Bild och länk -->
+<h1>Bloggar</h1>
+<div class="artist col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <div class="thumbnail">
+        <div class="caption">
+            <h3>Daniel Myrberg</h3>
+            <p>Info om bloggen</p>
+        </div>
+    </div>
+</div>
+<div class="artist col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <div class="thumbnail">
+        <div class="caption">
+            <h3>Jens Öhlund</h3>
+            <p>Info om bloggen</p>
+        </div>
+    </div>
+</div>
+<div class="artist col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <div class="thumbnail">
+        <div class="caption">
+            <h3>Lovisa Ohlson</h3>
+            <p>Info om bloggen</p>
+        </div>
+    </div>
+</div>
+<!-- -->
+
+<!-- En container per inlägg -->
+<div class="container">
+    <h1>Senaste 10 inläggen nedanför</h1>
+    <h1>...</h1>
+    <h1>...</h1>
+    <h1>...</h1>
+    <!-- Miniatyr av bloggaren, med namn -->
+    <div class="media">
+        <img class="media-object" src="">
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading">Daniel Myrberg</h4>
+        </div>
+    </div>
+    <!-- -->
+</div>
+<!--  -->
+
+
