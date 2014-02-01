680182d69b6f9e73c4c8878eff6161aa7b4d9109
Dennis <nygganh@nygganh.se>
2014-02-01 14:48:16 -0800

    Fixade limit?

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -174,7 +174,7 @@ class HomeController extends BaseController
 		}
 		else
 		{
-			$episodes = Episode::all()->take(10);
+			$episodes = Episode::take(10);
 		}
 
 		$this->assign('podcast', $podcast);
