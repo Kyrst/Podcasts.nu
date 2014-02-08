d3b0cfcf1d344bdf1263f710ec096331b7f97280
Kyrst <nygganh@nygganh.se>
2014-02-08 15:51:06 -0800

    Personliga sidan.

--- app/views/home/partials/get_episodes.php
+++ app/views/home/partials/get_episodes.php
@@ -6,14 +6,14 @@
 			</a>
 
 			<div class="media-body">
-				<h3 class="episode-head"><?php if ( $_podcast === NULL ): ?><a href="<?= $episode->podcast->getLink('avsnitt') ?>"><?= $episode->podcast->name ?></a> - <?php endif ?><a href="<?= $episode->getLink('avsnitt'); ?>"><?= $episode->title; ?></a> </h3>
+				<h3 class="episode-head"><?php if ( $_podcast === NULL ): ?><a href="<?= $episode->podcast->getLink('poddar') ?>"><?= $episode->podcast->name ?></a> - <?php endif ?><a href="<?= $episode->getLink('avsnitt'); ?>"><?= $episode->title; ?></a> </h3>
 				<p class="pub-date">(<?=date('Y-m-d H:i:s', $episode->pub_date) ?>)</p>
                 <div class="clear"></div>
 
 				<?= $episode->print_rater() ?>
 			</div>
 		<?php else: ?>
-			yoyo
+			Avsnittet saknar podd.
 		<?php endif ?>
 	</div>
 <?php endforeach; ?>
