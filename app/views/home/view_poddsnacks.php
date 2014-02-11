1ccba755eae2cea52f290193945d03941c7038f7
Kyrst <nygganh@nygganh.se>
2014-02-10 20:24:46 -0800

    Ett litet meddelande om inga poddsnack ar upplagda.

--- app/views/home/view_poddsnacks.php
+++ app/views/home/view_poddsnacks.php
@@ -1,15 +1,20 @@
 <h1>Poddsnack</h1>
 
-<?php foreach ( $podtalks as $podtalk ): ?>
-	<div id="podtalk_<?= $podtalk->id ?>" class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-		<div class="thumbnail text-center">
-			<a href="<?= $podtalk->get_link() ?>"><?= $podtalk->get_image('medium') ?></a>
+<?php if ( count($podtalks) > 0 ): ?>
+	<?php foreach ( $podtalks as $podtalk ): ?>
 
-			<div class="caption">
-				<h3><a href="<?= $podtalk->get_link() ?>"><?= $podtalk->title ?></a></h3>
-				<p><?= $podtalk->description ?></p>
+		<div id="podtalk_<?= $podtalk->id ?>" class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+			<div class="thumbnail text-center">
+				<a href="<?= $podtalk->get_link() ?>"><?= $podtalk->get_image('medium') ?></a>
+
+				<div class="caption">
+					<h3><a href="<?= $podtalk->get_link() ?>"><?= $podtalk->title ?></a></h3>
+					<p><?= $podtalk->description ?></p>
+				</div>
 			</div>
 		</div>
-	</div>
 
-<?php endforeach ?>
+	<?php endforeach ?>
+<?php else: ?>
+	<p>Inga poddsnack.</p>
+<?php endif ?>
