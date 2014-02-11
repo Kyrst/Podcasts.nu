1f9cfce7ac2637c06bc9ea9606e4e0e1e5871933
Kyrst <nygganh@nygganh.se>
2014-02-10 20:17:04 -0800

    Smaandring pa uppladdade bilder-sidan.

--- app/views/admin/uploaded_images.php
+++ app/views/admin/uploaded_images.php
@@ -11,8 +11,7 @@
 		<thead>
 			<tr>
 				<th></th>
-				<th>Name</th>
-				<th>Filename</th>
+				<th>Filnamn</th>
 				<th></th>
 			</tr>
 		</thead>
@@ -20,8 +19,11 @@
 			<?php foreach ( $uploaded_images as $image ): ?>
 				<tr>
 					<td><img src="<?= $image['url'] ?>" width="100" alt=""></td>
-					<td><?= $image['basename'] ?></td>
-					<td><?= $image['filename'] ?></td>
+					<td>
+						<strong><?= $image['basename'] ?></strong>
+						<br>
+						<?= $image['filename'] ?>
+					</td>
 					<td><a href="<?= URL::to('/admin/uppladdade-bilder/ta-bort/' . e($image['basename'])) ?>/<?= $image['type'] ?>" class="btn btn-primary delete-image">Ta bort</a></td>
 				</tr>
 			<?php endforeach ?>
