9009d42f7f8478a3c1b14950fb7b3c7cf0cac648
Kyrst <nygganh@nygganh.se>
2014-02-17 10:57:49 -0800

    Facebook...

--- app/config/facebook.php
+++ app/config/facebook.php
@@ -3,5 +3,5 @@ return array
 (
 	'FACEBOOK_APP_ID' => '360622727346863',
 	'FACEBOOK_APP_SECRET_KEY' => '871539d9d32f931b624d7fba014d8d7b',
-	'FACEBOOK_REDIRECT_URL' => urlencode('http://www.podcast.nu/facebook-login')
+	'FACEBOOK_REDIRECT_URL' => urlencode('http://www.podcasts.nu/facebook-login')
 );
--- app/controllers/FacebookController.php
+++ app/controllers/FacebookController.php
@@ -6,12 +6,14 @@ class FacebookController extends BaseController
 		$response = file_get_contents('https://graph.facebook.com/oauth/access_token?client_id=' . Config::get('facebook.FACEBOOK_APP_ID') . '&redirect_uri=' . Config::get('facebook.FACEBOOK_REDIRECT_URL') . '&client_secret=' . Config::get('facebook.FACEBOOK_APP_SECRET_KEY') . '&code=' . $code);
 
 		$params = NULL;
+
 		parse_str($response, $params);
 
 		$user = json_decode(file_get_contents('https://graph.facebook.com/me?access_token=' . $params['access_token']));
 
 		// Add user if not exists
-		if ( !$this->facebookIdExists($user->id) ) {
+		if ( !$this->facebookIdExists($user->id) )
+		{
 			$this->register($user->username, '', '', $user->first_name, $user->last_name, $user->id);
 		}
 
