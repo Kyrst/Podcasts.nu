afa029b0044fb4ba295cc678013e20bf28cb94f5
Kyrst <nygganh@nygganh.se>
2014-02-17 11:48:58 -0800

    Facebook...

--- app/controllers/FacebookController.php
+++ app/controllers/FacebookController.php
@@ -12,8 +12,6 @@ class FacebookController extends BaseController
 
 		$code = $input['code'];
 
-		//$convert = file_get_contents('https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=' . Config::get('facebook.FACEBOOK_APP_ID') . '&client_secret=' . Config::get('facebook.FACEBOOK_APP_SECRET_KEY') . '&fb_exchange_token=' . $code);
-
 		$response = file_get_contents('https://graph.facebook.com/oauth/access_token?client_id=' . Config::get('facebook.FACEBOOK_APP_ID') . '&redirect_uri=' . Config::get('facebook.FACEBOOK_REDIRECT_URL') . '&client_secret=' . Config::get('facebook.FACEBOOK_APP_SECRET_KEY') . '&code=' . $code);
 
 		$params = NULL;
