ecd463f01430d1d8138fa24266a7fe525b69b772
Kyrst <nygganh@nygganh.se>
2014-02-17 11:46:50 -0800

    Facebook...

--- app/controllers/FacebookController.php
+++ app/controllers/FacebookController.php
@@ -23,7 +23,7 @@ class FacebookController extends BaseController
 		$user = json_decode(file_get_contents('https://graph.facebook.com/me?access_token=' . $params['access_token']));
 
 		// Add user if not exists
-		die(var_dump($user->id));
+
 		if ( !$this->user_with_facebook_id_exists($user->id) )
 		{
 			$new_user = new User();
@@ -52,6 +52,7 @@ class FacebookController extends BaseController
 
 	private function user_with_facebook_id_exists($facebook_id)
 	{
+		die(var_dump(User::where('facebook_id', $facebook_id)->count()));
 		return (User::where('facebook_id', $facebook_id)->count() === 1);
 	}
 }
