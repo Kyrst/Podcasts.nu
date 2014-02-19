0204718bf5736a4b4c92e4f4b687aa1482bdc4ac
Kyrst <nygganh@nygganh.se>
2014-02-18 21:23:30 -0800

    Kommer ihag om man har spelaren oppen eller stangd nar man refreshar sidan + rewrite hannah.

--- app/routes.php
+++ app/routes.php
@@ -5,12 +5,12 @@ Route::get('/avsnitt/{podcast}/{episode}.htm', function($podcast, $episode)
 	return Redirect::to('/avsnitt/' . $podcast . '/' . $episode, 301);
 })->where(array('podcast', '[a-z0-9_\-]+'), array('episode', '[a-z0-9_\-]+'));
 
-Route::get('/poddar/co-hannah-och-amanda/', function()
+Route::get('/poddar/co-hannah-och-amanda', function()
 {
 	return Redirect::to('/poddar/fredagspodden-med-hannah-och-amanda', 301);
 });
 
-Route::get('/avsnitt/co-hannah-och-amanda/', function()
+Route::get('/avsnitt/co-hannah-och-amanda', function()
 {
 	return Redirect::to('/avsnitt/fredagspodden-med-hannah-och-amanda', 301);
 });
