d845ab1119bef9241fad8bb35179bc23112573c1
Myrberg <dmyrberg@gmail.com>
2014-02-22 16:37:40 +0100

    Fyllt i "info"-sidan

--- app/views/home/info.php
+++ app/views/home/info.php
@@ -1,13 +1,58 @@
 <h1>Info</h1>
+<h2>Kontakt</h2>
+<div id="kontakt" class="lg-12 md-12 sm-12 xs-12">
+<h3>Allmänna frågor:</h3>
+    <p class="info-short"><a>kontakt@podcasts.nu</a></p>
 
-<div id="info" style="height:300px">info</div>
-
-<div id="kontakt" style="height:300px">kontakt</div>
-
-<div id="spelaren" style="height:300px">Nya spelaren</div>
-
-<div id="anvandarvillkor" style="height:300px">Användarvillkor</div>
-
+<h3>Tips om nya podcasts, frågor om hemsidan:</h3>
+    <p class="info-short"><a>daniel@podcasts.nu</a></p>
 
 
-<div id="cookies" style="height:300px">cookies</div>
+<h3>Frågor angående våra sociala medier</h3>
+    <p class="info-short"><a>jens@podcasts.nu</a></p>
+</div>
+
+<div id="info">
+    <h2>Om</h2>
+<p>Podcasts.nu! Vi finns till för att göra det enklare för dig att lyssna på dina nuvarande favorit-podcasts men också
+    underlätta för dig att hitta nya intressanta podcasts. Vi samlar olika Svenska podcasts, både stora och de lite mindre
+    kända. Som icke inloggad besökare hjälper vi dig med att komma ihåg var i din befintliga spelning du är, så om du byter sida etc kommer vi ihåg var du var innan det bröts tack vare cookies, läs mer om cookies längst ner</p>
+
+<p class="info-short">Fördelar som medlem</p>
+    <p>Som medlem får du en hel del fler funktioner kring dina spelningar. Vi kommer inte bara ihåg din senaste
+        spelning vi kommer även ihåg alla dina spelningar.</p>
+    <p>Allt presenteras under "Min Sida" som du kommer åt när du klickar på ditt namn i menyn, som inloggad.
+        Där ser du alla dina senaste spelningar, vilka spelningar som ej är avslutade, samt vilka podcasts du prenumererar på.
+        Under tabellen "Ej lyssnat klart" kan du enkelt se hur långt du har kvar av varje avsnitt. Här kan du även starta avsnittet därifrån du var senast.
+        Om du känner att du är klar med avsnittet klickar du bara på "markera som färdiglyssnad".
+        <p class="info-short">Statuskoder för avsnitt:</p>
+
+    <p class="label label-danger">Lyssna nu</p>
+    <p>Denna visar att du inte påbörjat någon spelning av avsnittet.</p>
+
+    <span class="label label-warning">Påbörjad</span>
+    <p>Denna ser du när avsnittet nån gång har börjat spelats. avsnittet finns då även med under min sida där du kan starta från där du slutade sist.
+    Du kan även gå in på avsnittet från vilken plats på sidan som helt och trycka på play för börja spela där du var senast.</p>
+
+    <span class="label label-success">Lyssnad</span>
+    <p> Denna innebär att du lyssnat klart på avsnittet. Vi har själva valt att aldrig automatiskt avgöra om ett avsnitt
+        är färdigspelat eller inte. Det avgör du själv, antingen via min sida där du kan välja "markera som färdiglyssnad". Eller direkt nere i spelaren
+        när ett avsnitt spelas, då trycker du på den gröna bocken, till höger om play/pause.
+</p>
+    <p>Besök gärna "Min Sida" och upptäck det smidiga fördelarna med att vara medlem!</p>
+    <p><a href="/sign-up" class="btn btn-primary btn-sm" role="button">Bli medlem</a></p>
+</div>
+
+<div id="anvandarvillkor">
+    <h2>Användarvillkor</h2>
+
+</div>
+
+
+
+<div id="cookies">
+    <h2>Cookies</h2>
+    En Cookie är en liten textfil som lagras i besökarens dator. Denna textfil kommer endast innehålla information för att underlätta
+    för just ditt användande. Exempelvis var någonstans i ett avsnitt du är. För att vi på så sätt ska kunna erbjuda en bättre och mer
+    lättanvänlig hemsida.
+</div>
--- app/views/home/sign_up.php
+++ app/views/home/sign_up.php
@@ -78,3 +78,24 @@
 		</div>
 	</div>
 </form>
+<p class="info-short">Fördelar som medlem</p>
+<p>Som medlem får du en hel del fler funktioner kring dina spelningar. Vi kommer inte bara ihåg din senaste
+    spelning vi kommer även ihåg alla dina spelningar.</p>
+<p>Allt presenteras under "Min Sida" som du kommer åt när du klickar på ditt namn i menyn, som inloggad.
+    Där ser du alla dina senaste spelningar, vilka spelningar som ej är avslutade, samt vilka podcasts du prenumererar på.
+    Under tabellen "Ej lyssnat klart" kan du enkelt se hur långt du har kvar av varje avsnitt. Här kan du även starta avsnittet därifrån du var senast.
+    Om du känner att du är klar med avsnittet klickar du bara på "markera som färdiglyssnad".
+<p class="info-short">Statuskoder för avsnitt:</p>
+
+<p class="label label-danger">Lyssna nu</p>
+<p>Denna visar att du inte påbörjat någon spelning av avsnittet.</p>
+
+<span class="label label-warning">Påbörjad</span>
+<p>Denna ser du när avsnittet nån gång har börjat spelats. avsnittet finns då även med under min sida där du kan starta från där du slutade sist.
+    Du kan även gå in på avsnittet från vilken plats på sidan som helt och trycka på play för börja spela där du var senast.</p>
+
+<span class="label label-success">Lyssnad</span>
+<p> Denna innebär att du lyssnat klart på avsnittet. Vi har själva valt att aldrig automatiskt avgöra om ett avsnitt
+    är färdigspelat eller inte. Det avgör du själv, antingen via min sida där du kan välja "markera som färdiglyssnad". Eller direkt nere i spelaren
+    när ett avsnitt spelas, då trycker du på den gröna bocken, till höger om play/pause.
+</p>
